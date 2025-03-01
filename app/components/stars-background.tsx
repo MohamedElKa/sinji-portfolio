0645803b2a01e163d6b1c10f9a95930631116e07
MohamedElka <simokarim2099@gmail.com>
2025-03-01 22:29:56 +0000

    dark mode done download resume done still responsiveness and language change so its 65% done

--- app/components/stars-background.tsx
+++ app/components/stars-background.tsx
@@ -37,7 +37,7 @@ export const StarsBackground: React.FC<StarBackgroundProps> = ({
   bodyHeight
 }) => {
   const [stars, setStars] = useState<StarProps[]>([]);
-  const canvasRef: RefObject<HTMLCanvasElement> =
+  const canvasRef: RefObject<HTMLCanvasElement | null> =
     useRef<HTMLCanvasElement>(null);
 
   const generateStars = useCallback(
